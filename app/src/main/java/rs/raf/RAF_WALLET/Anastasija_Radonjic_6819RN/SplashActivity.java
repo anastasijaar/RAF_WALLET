2e68fb5fce8e49d01bf0e0f73ca01bec74a962b1
Anastasija Radonjic <anastasijaradonjic99@gmail.com>
2021-04-11 23:55:14 +0200

    Dodavanje splash screen-a.

--- app/src/main/java/rs/raf/RAF_WALLET/Anastasija_Radonjic_6819RN/SplashActivity.java
+++ app/src/main/java/rs/raf/RAF_WALLET/Anastasija_Radonjic_6819RN/SplashActivity.java
@@ -2,15 +2,17 @@ package rs.raf.RAF_WALLET.Anastasija_Radonjic_6819RN.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import rs.raf.RAF_WALLET.Anastasija_Radonjic_6819RN.R;
-
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
